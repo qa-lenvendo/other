5db71afd1a8608ea51a5367beec400caf2db4542
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-04-27 13:44:21 +0300

    add home work 5.5

--- test_homework.py
+++ test_homework.py
@@ -1,4 +1,5 @@
 from selene import browser, have, be
+import os
 
 def test_homework():
 
@@ -8,7 +9,7 @@ def test_homework():
     browser.element('[id="lastName"]').send_keys('Ivanov')
     browser.element('[id="userEmail"]').send_keys('r@ya.com')
     browser.element('[name="gender"][value=Male]').double_click()
-    browser.element('[id="userNumber"]').send_keys('+7777777777')
+    browser.element('[id="userNumber"]').send_keys('7777777777')
 
     browser.element('[id="dateOfBirthInput"]').click()
     browser.element('.react-datepicker__year-select').click().element('[value="1990"]').click()
@@ -17,14 +18,21 @@ def test_homework():
 
     browser.element('#subjectsInput').click().send_keys('Maths').press_enter()
 
-    browser.element('[id="hobbies-checkbox-1"]').double_click()
+    browser.element('label[for="hobbies-checkbox-1"]').click()
 
-    # browser.element('id="uploadPicture"').click()
+    browser.element('#uploadPicture').send_keys(os.getcwd() + '/test.png')
 
     browser.element('[id="currentAddress"]').send_keys('Test-city, test street, test house 2')
 
-    browser.element('[id="state"]').click()
-    browser.element('[id="state"]').send_keys('U').press_enter()
+    browser.element('#state').click()
+    browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('NCR')).click()
+    browser.element('#react-select-4-input').type('Delhi').press_enter()
 
+    browser.element('#submit').press_enter()
 
-    ...
+    browser.all('tbody tr').should(have.exact_texts(
+        'Student Name Ivan Ivanov', 'Student Email r@ya.com', 'Gender Male', 'Mobile 7777777777',
+        'Date of Birth 22 March,1990', 'Subjects Maths', 'Hobbies Sports',
+        'Picture test.png', 'Address Test-city, test street, test house 2',
+        'State and City NCR Delhi'))
+
